029c27a2446b9118e1eee90ef11086a188d30612
sangpk <dinhsangcnc@gmail.com>
2022-07-15 11:30:33 +0700

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+// let name=prompt("xuất ra tên người dùng");
+// alert("xin chào tôi tên sang");
+// console.log(" năm nay tôi 30 tuổi");
+// document.write("tôi sống ở hà nội");
+// document.getElementById("thong tin").innerHTML=name
+<form>
+    user name : <br/>
+    <input type="text" name="user name"><br/>
+        user password: <br/>
+        <input type=
+
+</form>
